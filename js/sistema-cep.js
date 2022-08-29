8106b76a3787f286594e50afdef3ce79636b2957
Victor <vmtame@gmail.com>
2022-08-29 20:42:01 -0300

    wip: aula 29-ago-2022 => 002

--- js/sistema-cep.js
+++ js/sistema-cep.js
@@ -2,14 +2,21 @@ function search() {
     const zip = document.getElementById("zipcode").value ;
 
     fetch(`https://api.postmon.com.br/v1/cep/${zip}`).then(response => {
-        console.log(response.json());
-        // return response.json();
+        // console.log(response.json());
+        return response.json();
     }).then(response => {
-        // document.getElementById("cep").innerHTML = response.cep;
-        // document.getElementById("address").innerHTML = response.logradouro;
+        document.getElementById("zip").value = response.cep;
+        document.getElementById("neighbourhood").value = response.bairro;
+        document.getElementById("state").value = response.estado;
+        document.getElementById("city").value = response.cidade;
+        document.getElementById("address").value = response.logradouro;
+
     });
 }
 
 function clean() {
-    document.getElementsByClassName('clean').value = "";
+    const group = document.getElementsByClassName('clean');
+    for(let i = 0; i < group.length; i++) {
+        group[i].value = ""
+    }
 }
